7c030f89bc1670d0686cd6f1d86feb4af1943eae
Gareth Johns <info@garethjohnsdesign.com>
2019-09-09 16:21:15 +0100

    Player Tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,52 +69,18 @@ $('a[href*="#"]')
 
 const controls = `
 <div class="plyr__controls">
+    <div class="plyr__control">
+      {title}
+    </div>
     <button type="button" class="plyr__control next-audio-item">
-        <svg role="presentation"><use xlink:href="#plyr-rewind"></use></svg>
-        <span class="plyr__tooltip" role="tooltip">Rewind {seektime} secs</span>
+        <svg class="icon icon-arrow_forward_ios"><use xlink:href="/assets/icons/symbol-defs.svg#icon-arrow_forward_ios"></use></svg>
     </button>
-    <button type="button" class="plyr__control" aria-label="Play, {title}" data-plyr="play">
-        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-pause"></use></svg>
-        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-play"></use></svg>
-        <span class="label--pressed plyr__tooltip" role="tooltip">Pause</span>
-        <span class="label--not-pressed plyr__tooltip" role="tooltip">Play</span>
+    <button type="button" class="plyr__control prev-audio-item">
+        <svg class="icon icon-arrow_back_ios"><use xlink:href="/assets/icons/symbol-defs.svg#icon-arrow_back_ios"></use></svg>
     </button>
-    <button type="button" class="plyr__control next-audio-item">
-        <svg role="presentation"><use xlink:href="#plyr-fast-forward"></use></svg>
-        <span class="plyr__tooltip" role="tooltip">Forward {seektime} secs</span>
-    </button>
-    <div class="plyr__progress">
-        <input data-plyr="seek" type="range" min="0" max="100" step="0.01" value="0" aria-label="Seek">
-        <progress class="plyr__progress__buffer" min="0" max="100" value="0">% buffered</progress>
-        <span role="tooltip" class="plyr__tooltip">00:00</span>
-    </div>
-
-    <div>
-        {title}
-    </div>
-
-    <div class="plyr__time plyr__time--current" aria-label="Current time">00:00</div>
-    <div class="plyr__time plyr__time--duration" aria-label="Duration">00:00</div>
-    <button type="button" class="plyr__control" aria-label="Mute" data-plyr="mute">
-        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-muted"></use></svg>
-        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-volume"></use></svg>
-        <span class="label--pressed plyr__tooltip" role="tooltip">Unmute</span>
-        <span class="label--not-pressed plyr__tooltip" role="tooltip">Mute</span>
-    </button>
-    <div class="plyr__volume">
-        <input data-plyr="volume" type="range" min="0" max="1" step="0.05" value="1" autocomplete="off" aria-label="Volume">
-    </div>
-    <button type="button" class="plyr__control" data-plyr="captions">
-        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-captions-on"></use></svg>
-        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-captions-off"></use></svg>
-        <span class="label--pressed plyr__tooltip" role="tooltip">Disable captions</span>
-        <span class="label--not-pressed plyr__tooltip" role="tooltip">Enable captions</span>
-    </button>
-    <button type="button" class="plyr__control" data-plyr="fullscreen">
-        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-exit-fullscreen"></use></svg>
-        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-enter-fullscreen"></use></svg>
-        <span class="label--pressed plyr__tooltip" role="tooltip">Exit fullscreen</span>
-        <span class="label--not-pressed plyr__tooltip" role="tooltip">Enter fullscreen</span>
+    <button type="button" class="plyr__control play-audio-item" aria-label="Play, {title}" data-plyr="play">
+        <svg class="icon--pressed icon icon-pause"><use xlink:href="/assets/icons/symbol-defs.svg#icon-pause"></use></svg>
+        <svg class="icon--not-pressed icon icon-play_arrow"><use xlink:href="/assets/icons/symbol-defs.svg#icon-play_arrow"></use></svg>
     </button>
 </div>
 `;
@@ -123,21 +89,6 @@ const controls = `
 const player = window.pl = new Plyr('#player', {
   controls
 });
-
-player.source = {
-    type: 'audio',
-    title: 'Example title',
-    sources: [
-        {
-            src: '/uploads/test-02.mp3',
-            type: 'audio/mp3',
-        },
-        {
-            src: '/uploads/test-02.ogg',
-            type: 'audio/ogg',
-        },
-    ],
-};
 
 function playSrc (url, $source) {
   //console.log(url)
