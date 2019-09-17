5d3cac5f53234a68959dfcf47fce2f4ea5e342d0
Gareth Johns <garethjohns@Gareths-iMac.local>
2019-09-17 18:25:35 +0100

    Initial Commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -193,3 +193,11 @@ var $this = $(this)
    e.preventDefault()
    return false;
 })
+
+
+var video = document.getElementById("video_background");
+video.addEventListener("canplay", function() {
+  setTimeout(function() {
+    video.play();
+  }, 3000);
+});
