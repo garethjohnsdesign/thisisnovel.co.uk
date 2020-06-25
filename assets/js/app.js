77dd7da659a28073a89869434a99a6745fab6548
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-06-25 14:29:05 +0100

    Update Packages

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,22 +34,6 @@ if ($("body").hasClass("home")) {
   }, 3000
   );
 }
-// 1. Scroll Hide Navigation
-// ---------------
-
-/*
-var prev = 0;
-var $window = $(window);
-var nav = $('.scrollhide-nav');
-
-$window.off('scroll');
-$window.on('scroll', function() {
-    var scrollTop = $window.scrollTop();
-    if (scrollTop < 0) scrollTop = 0;
-    nav.toggleClass('hidden', scrollTop > prev);
-    prev = scrollTop;
-});
-*/
 
 
 // 8. Animate on Scroll
